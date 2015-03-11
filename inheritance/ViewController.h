69ee8caabe4adc8eb2628f387fb7fbe86a6d217d
mateo <mateo@itdepts-MacBook-Air.local>
2015-03-11 16:08:00 +0800

    Initial Commit

---
+++ inheritance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  inheritance
+//
+//  Created by mateo on 11/3/15.
+//  Copyright (c) 2015 mateo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
