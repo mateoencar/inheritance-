7819bc5b847639f49eef4a36c67a85f41c3a87df
mateoencar <encar7435@gapps.uwcsea.edu.sg>
2015-04-30 23:53:29 +0800

    added a test class

---
+++ inheritance/huh.h
@@ -0,0 +1,13 @@
+//
+//  huh.h
+//  inheritance
+//
+//  Created by mateo on 30/4/15.
+//  Copyright (c) 2015 mateo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface huh : NSObject
+
+@end
---
+++ inheritance/huh.m
@@ -0,0 +1,13 @@
+//
+//  huh.m
+//  inheritance
+//
+//  Created by mateo on 30/4/15.
+//  Copyright (c) 2015 mateo. All rights reserved.
+//
+
+#import "huh.h"
+
+@implementation huh
+
+@end
